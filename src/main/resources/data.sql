693fc9ce15ce0a78747bdee2b176170ed3e4b980
Gautam Kumar <17165349+gauti04@users.noreply.github.com>
2025-12-30 14:47:55 +0530

    Initialized schema and seeded with some data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO holidays (name, date, country) VALUES
+('New Year', '2025-01-01', 'India'),
+('Republic Day', '2025-01-26', 'India'),
+('Gandhi Jayanti', '2025-10-02', 'India'),
+('Dussehra', '2025-10-02', 'India');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS holidays;
+
+CREATE TABLE holidays (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    country VARCHAR(100)
+);
